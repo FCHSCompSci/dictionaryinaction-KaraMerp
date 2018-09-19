155a59438b65bb06a6caba122f79fde27e143f8c
Kara Michelle Morgan <kara.morgan@stu.jefferson.kyschools.us>
2018-09-19 08:29:41 -0400

    push

--- dictionary.py
+++ dictionary.py
@@ -6,41 +6,40 @@ shirt = []
 tanktop = []
 
 order = input("Would you like to place a order?: ")
+orders = Hoody + shirt + tanktop
 
 if order == "no":
     print("Bye!")
 else:
     print("For a Hoodie use the first funtion, for a Shirt use the second function, and for a Tanktop use the third function. ")
-    
-    def first(size, color, design):
-        Hoody={
-            'size' : size,
-            'color' : color,
-            'design' : design
-            }
-        hoody.append(Hoody)
-        return "Thank you for your Hoodie order" 
+    while order == "yes":   
+        def first(size, color, design):
+            Hoody={
+                'size' : size,
+                'color' : color,
+                'design' : design
+                }
+            hoody.append(Hoody)
+            return "Thank you for your Hoodie order" 
 
 
-    def second(size, color, design):
-        shirt={
-            'size' : size,
-            'color' : color,
-            'design' : design
-            }
-        shirt.append(shirt)
-        return "thank you for your Shirt order"
+        def second(size, color, design):
+            shirt={
+                'size' : size,
+                'color' : color,
+                'design' : design
+                }
+            shirt.append(shirt)
+            return "thank you for your Shirt order"
 
-    def third(size, color, design):
-        tanktop={
-            'size' : size,
-            'color' : color,
-            'design' :  design
-            }
-        tanktop.append(tanktop)
-        return "Thank you for your Tanktop order"
+        def third(size, color, design):
+            tanktop={
+                'size' : size,
+                'color' : color,
+                'design' :  design
+                }
+            tanktop.append(tanktop)
+            return "Thank you for your Tanktop order"
 
-orders = Hoody + shirt + tanktop
-print(orders)
 
 
