8c3f8ce4b283ff9a3ca8d8c30139540c0752f7c6
Kara Michelle Morgan <kara.morgan@stu.jefferson.kyschools.us>
2018-09-26 07:18:25 -0400

    done

--- dictionary.py
+++ dictionary.py
@@ -24,11 +24,13 @@ while True:
     user_input = input("Would you like to order a [s]hirt, [p]ickup or [e]xit? ")
     if user_input == "s":
         order_name = input("What is the name for the order? ")
-        order_name== make_customer(shirt_id, 'name')
+        s_name = make_customer(shirt_id, order_name)
         shirt_id = shirt_id + 1
         shirt_size = input("What size would you like? ")
         shirt_color = input("What color would you like? ")
         shirt_design = input("What design would you like? ")
+        whole = make_shirt(shirt_id, shirt_size, shirt_color, shirt_design)
+          
     elif user_input == "p":
         pass
     elif user_input == "e":
